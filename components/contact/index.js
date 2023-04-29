f7cb5a099ca96664bb8099ed231f3083d1420de7
jahdevelops <elijaheze777@gmail.com>
2023-04-29 08:42:47 -0700

    add form components

--- components/contact/index.js
+++ components/contact/index.js
@@ -1,12 +1,30 @@
 
+function Field({children}) {
+	return <div className="field flex w-full justify-between">
+		{children}
+	</div>
+}
+
+function Input(props) {
+	return <input {...props} className="" />
+}
+
 export default function () {
 	return (
 		<section id="contact">
 			<h2 className="h2_header"> Contact me </h2>
-			<form className="">
+			<form className="" id="contact_form">
 				<legend className="semifaded_text"> Feel free, leave me a message lets discuss your next project together. </legend>
-				<fieldset>
-
+				<fieldset className="mt-5">
+					<Field>
+						<Input type="text" placeholder="Name" required />
+						<Input type="email" placeholder="Email" required />
+					</Field>
+					<Field>
+						<textarea>
+							Leave me a message
+						</textarea>
+					</Field>
 				</fieldset>
 			</form>
 		</section>
