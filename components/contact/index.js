e8356975d50ab6ed9588cf414bdab7adcae116f4
jahdevelops <elijaheze777@gmail.com>
2023-04-29 08:25:29 -0700

    add attributes

--- components/contact/index.js
+++ components/contact/index.js
@@ -2,7 +2,13 @@
 export default function () {
 	return (
 		<section id="contact">
+			<h2 className="h2_header"> Contact me </h2>
+			<form className="">
+				<legend className="semifaded_text"> Feel free, leave me a message lets discuss your next project together. </legend>
+				<fieldset>
 
+				</fieldset>
+			</form>
 		</section>
 	)
 }
