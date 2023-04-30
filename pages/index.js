2860829f66682591320fb9773c410120a98ba2c8
jahdevelops <elijaheze777@gmail.com>
2023-04-29 21:16:12 -0700

    re-order elements

--- pages/index.js
+++ pages/index.js
@@ -27,36 +27,38 @@ export default function Home() {
 				<title>jahdevelops</title>
 			</Head>
 
-			<section className='px-5 mt-[2rem]' id='main'>
-				<div className='flex justify-between items-center'>
-					<div className='flex-1 text-center sm:text-left sm:items-start flex flex-col items-center'>
-						<h1 className='text-[1.7rem] font-medium'>Hi all ✋, I am Elijah Eze</h1>
-						<p className='semifaded_text mt-[.5rem]'>
-							A passionate fullstack developer, having an experience of building Web and Mobile applications with JavaScript, Nodejs, React, React-Native and some other cool libraries and frameworks.
-						</p>
-						<div className='my-5 flex'>
-							<Social href='https://github.com/jahdevelops'><FaGithub className='text-[#181717]' /> </Social>
-							<Social href='https://linkedin.com/elijh-e'><FaLinkedin className='text-[#0077B5]' /> </Social>
-							<Social href='mailto:elijaheze777@gmail.com'><AiFillGoogleCircle className='text-[#DB4437]' /> </Social>
-							<Social href='https://api.whatsapp.com/send?phone=09155977113&text=Hello%Elijah!'><AiOutlineWhatsApp className='text-[#25D366]' /> </Social>
+			<section className='px-[1rem]' id='main'>
+				<section>
+					<div className='flex justify-between items-center'>
+						<div className='flex-1 mb-[1rem] sm:mr-[2rem] text-center sm:text-left sm:items-start flex flex-col items-center'>
+							<h1 className='text-[1.7rem] mb-[.5rem] font-medium'>Hi all ✋, I am Elijah Eze</h1>
+							<p className='semifaded_text mt-[.5rem]'>
+								A passionate fullstack developer, having an experience of building Web and Mobile applications with JavaScript, Nodejs, React, React-Native and some other cool libraries and frameworks.
+							</p>
+							<div className='my-[2rem] flex'>
+								<Social href='https://github.com/jahdevelops'><FaGithub className='text-[#181717]' /> </Social>
+								<Social href='https://linkedin.com/elijh-e'><FaLinkedin className='text-[#0077B5]' /> </Social>
+								<Social href='mailto:elijaheze777@gmail.com'><AiFillGoogleCircle className='text-[#DB4437]' /> </Social>
+								<Social href='https://api.whatsapp.com/send?phone=09155977113&text=Hello Elijah!'><AiOutlineWhatsApp className='text-[#25D366]' /> </Social>
+							</div>
+							<div className='mt-[2rem] flex'>
+								<a 
+									href=''
+									download
+									className='flex items-center justify-center mr-2 text-center text-white bg_green w-[130px] py-2 rounded-md'>Resume &nbsp; <FaFileDownload /></a>
+								
+							</div>
 						</div>
-						<div className='mt-[1.3rem] flex'>
-							<a 
-								href=''
-								download
-								className='flex items-center justify-center mr-2 text-center text-white bg_green w-[130px] py-2 rounded-md'>Resume &nbsp; <FaFileDownload /></a>
-							
+						<div className='hidden sm:block w-[40%]'>
+							<Image 
+								className='w-full h-[200px]'
+								src={'/undraw_developer_activity_re_39tg.svg'} 
+								width={'360px'}
+								height={'360px'}
+							/>
 						</div>
 					</div>
-					<div className='hidden sm:block'>
-						<Image 
-							className='w-full h-[200px]'
-							src={'/undraw_developer_activity_re_39tg.svg'} 
-							width={'360px'}
-							height={'360px'}
-						/>
-					</div>
-				</div>
+				</section>
 
 				<Projects />
 				<ContactForm />
