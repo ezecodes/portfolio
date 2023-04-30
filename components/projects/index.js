262c94c02a7ed6213869b44823e41a4bba6b3126
jahdevelops <elijaheze777@gmail.com>
2023-04-29 23:16:56 -0700

    add project cards

--- components/projects/index.js
+++ components/projects/index.js
@@ -1,8 +1,65 @@
+import { FaBookOpen, FaGithubAlt, FaWindowMaximize } from "react-icons/fa"
+
+function ProjectCard({name, description, img, url, type}) {
+	return (
+		<div className="project_card flex justify-center flex-col h-[350px] sm:mr-0 px-5 shadow-md mb-[2rem] ">
+			<div> <img src={img.url} /> </div>
+			<div className="mt-5">
+				<h3 className="text-lg mb-2 font-semibold">{name}</h3>
+				<p className="semifaded_text">{description}</p>
+			</div>
+			<div className="mt-3">
+				{
+					type === 'public' ?
+					<a href={url} rel="noreferrer" target="_blank" className="card_link border-[#ffd2bb]" >
+						<FaGithubAlt className="text-[#ff9056]" />
+						<span className="ml-3 text-[#ff9056]"> View on Github </span>
+					</a>
+					:
+					<a href={url} rel="noreferrer" target="_blank" className="card_link border-[#868593]" >
+						<span className="mr-3 text-[#3f3d56]"> Open </span>
+						<FaBookOpen className="text-[#3f3d56]" />
+					</a>
+				}
+			</div>
+		</div>
+	)
+}
+
+const projects = [
+	{
+		name: 'mobipay',
+		description: 'A financial application used by company investors to give out and receive money from prospective customers.',
+		img: {
+			url: '/project_thumbnails/mobipay.png'
+		},
+		type: 'private',
+		url: 'https://gomobidata.com'
+	},
+	{
+		name: 'mobipay',
+		description: 'A financial application used by company investors to give out and receive money from prospective customers.',
+		img: {
+			url: '/project_thumbnails/mobipay.png'
+		},
+		type: 'public',
+		url: 'https://github.com/jahdevelops'
+	}
+]
 
 export default function () {
 	return (
 		<section id="projects">
-
+				<h2 className="h2_header"> <span>Projects</span> </h2>
+				<div className="flex justify-between flex-col sm:flex-row">
+					{
+						projects.map((item, i) => {
+							return (
+								<ProjectCard key={i} {...item} />
+							)
+						})
+					}
+				</div>
 		</section>
 	)
 }
