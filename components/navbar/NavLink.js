5d8db466a8fae3930fae64ff7c9e83d60826b80b
jahdevelops <elijaheze777@gmail.com>
2023-05-01 22:29:56 -0700

    modify path logic

--- components/navbar/NavLink.js
+++ components/navbar/NavLink.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 function NavLink({path, icon, className}) {
     return (
-        <Link passHref href={`/#${ path.toLowerCase() === 'home' ? '' : path }`} >
+        <Link passHref href={path.toLowerCase() === 'home' ? '/' : `/#${path}`} >
             <a> {icon && icon} {path}</a>
         </Link>
     )
