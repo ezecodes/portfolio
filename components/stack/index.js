4a83a9515ad06491ba791eeac8ad92f1c053457b
jahdevelops <elijaheze777@gmail.com>
2023-05-01 21:18:14 -0700

    add stack array

--- components/stack/index.js
+++ components/stack/index.js
@@ -1,9 +1,37 @@
 
+// import { TbBrandTypescript } from "react-icons/tb";
+import { IoLogoJavascript } from "react-icons/io";
+import { FaNodeJs, FaReact } from 'react-icons/fa';
+import { SiMysql, SiNextdotjs } from 'react-icons/si'
+import { DiMongodb } from 'react-icons/di'
+
+const stacks = [
+	['nextjs', <SiNextdotjs className="text-[#00]" />],
+	// ['typescript', <TbBrandTypescript className="text-[#007ACC]" />],
+	['javascript', <IoLogoJavascript className="text-[#F7DF1E]" />],
+	['reactjs', <FaReact className="text-[#61DAFB]" />],
+	['nodejs', <FaNodeJs className="text-[#43853d]"/>],
+	['sql', <SiMysql className="text-[#00758F]" />],
+	['mongodb', <DiMongodb className='text-[#4DB33D]' />]
+]
+
 export default function () {
 	return (
 		<section id="stack">
 			<h2 className="h2_header"> <span>Stack</span> </h2>
-
+			<p className="semifaded_text">These are the plethora of languages and frameworks I have used throughout my programming career.</p>
+			<div className="grid grid-cols-4 row-span-1	gap-4 sm:grid-cols-8 mt-5 ">
+				{
+					stacks.map((stack, i) => {
+						return (
+							<div className="stack" key={i}>
+								<span>{stack[1]}</span>
+								<span className="semifaded_text"> {stack[0]} </span>
+							</div>
+						)
+					})
+				}
+			</div>
 		</section>
 	)
 }
