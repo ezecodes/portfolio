9bdfd9d00dba67b8869a9b1ccf33482cd6be28da
jahdevelops <elijaheze777@gmail.com>
2023-05-02 19:18:52 -0700

    send form body to email address

---
+++ pages/api/form.js
@@ -0,0 +1,38 @@
+import {SMTPClient} from 'emailjs'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const client = new SMTPClient({
+    user: process.env.mailAddress,
+    password: process.env.mailPass,
+    host: 'smtp.gmail.com',
+    ssl: true
+})
+
+export default async function handleUserForm(req, res) {
+    const {method, body} = req
+
+    if (method === 'POST') {
+        const {email, name, msg} = req.body
+        
+        const compose = {
+            from: process.env.mailAddress,
+            to: process.env.mailAddress,
+            subject: 'Form from portfolio site',
+            text: `Email: ${email}, name: ${name}, message: ${msg}`
+        }
+        client.send(compose, (err, data) => {
+            if (err) {
+                console.error(err)
+                return res.status(500).json({
+                    success: false
+                })
+            }
+            console.log(data)
+            res.status(200).json({
+                success: true,
+                msg: 'Form received'
+            })
+        })
+    }
+}
