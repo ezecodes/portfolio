f9e85a06ef3e09beab1651df46563911ecc39f9a
jahdevelops <elijaheze777@gmail.com>
2023-05-02 20:54:23 -0700

    modify toast handler

--- components/contact/index.js
+++ components/contact/index.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
