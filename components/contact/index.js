35a359db0ddb60c16f19b7314586bd5a69ff8ff6
jahdevelops <elijaheze777@gmail.com>
2023-05-02 20:29:30 -0700

    add logic for form handler

--- components/contact/index.js
+++ components/contact/index.js
@@ -1,3 +1,6 @@
+import { useRef, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Field({children}) {
 	return <div className="field flex w-full flex-col sm:flex-row justify-between">
@@ -5,29 +8,67 @@ function Field({children}) {
 	</div>
 }
 
-function Input(props) {
-	return <input {...props} className="" />
-}
-
 export default function () {
-	return (
+	const nameRef = useRef(null)
+	const emailRef = useRef(null)
+	const msgRef = useRef(null)
+	const [disabled, setButtonState] = useState(false)
+
+	function handleForm(e) {
+		e.preventDefault()
+		const [email, name, msg] = [emailRef.current.value, nameRef.current.value, msgRef.current.value]
+		setButtonState(true)
+		fetch('/api/form', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({email, name, msg})
+		})
+		.then(res => {
+			toast.success('Message received.')
+			emailRef.current.value = ''
+			nameRef.current.value = ''
+			msgRef.current.value = ''
+		})
+		.catch(err => {
+			console.error(err)
+		})
+		.finally(() => setButtonState(false))
+	}
+	return (<>
+		<ToastContainer
+			position="top-right"
+			autoClose={5000}
+			hideProgressBar={false}
+			newestOnTop={false}
+			closeOnClick
+			rtl={false}
+			pauseOnFocusLoss
+			draggable={false}
+			pauseOnHover
+		/>
 		<section id="contact">
 			<h2 className="h2_header"> <span>Contact me</span> </h2>
-			<form className="" id="contact_form">
+			<form className="" id="contact_form" onSubmit={handleForm}>
 				<legend className="semifaded_text text-center"> Feel free, leave me a message lets discuss your next project together. </legend>
 				<fieldset className="my-5">
 					<Field>
-						<Input type="text" placeholder="Name" required />
-						<Input type="email" placeholder="Email" required />
+						<input type="text" placeholder="Name" required ref={nameRef} />
+						<input type="email" placeholder="Email" required ref={emailRef} />
 					</Field>
 					<Field>
-						<textarea defaultValue='Your message here' />
+						<textarea placeholder='Your message here' ref={msgRef} />
 					</Field>
 				</fieldset>
 				<fieldset>
-					<Field><button className="bg_green text-white w-[100px] py-2 rounded-md">Submit</button></Field>
+					<Field>
+						<button disabled={disabled} className={`${disabled ? 'bg_lightgreen' : 'bg_green'} text-white w-[100px] py-2 rounded-md`}>
+							Submit {disabled && '...'}
+						</button>
+					</Field>
 				</fieldset>
 			</form>
-		</section>
+		</section></>
 	)
 }
